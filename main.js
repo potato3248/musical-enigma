ab781c665a5d3162c7116a39ebd95437c06ec34b
potato3248 <128443827+potato3248@users.noreply.github.com>
2024-03-21 18:44:13 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 550);
+    canvas.position(560,150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized!')
+}
+
+function draw() {
+    background('#969A97');
+
+    
+}
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+
+        noseX = results[0].pose.nose.x;
+        noseX = results[0].pose.nose.y;
+        console.log("noseX = " + noseX +" noseY=" + noseY)
+
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.leftWrist.x;
+        difference = leftWriteX - rightWrist;
+
+        console.log("leftWristX =" + leftWristX + " rightWristX = "+ rightWristX + "difference =" + difference);
+    }
+}
